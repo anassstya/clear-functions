63dcfe6706536b3ed3f590ae5225578f8d9873ba
anassstya <408183@edu.itmo.ru>
2024-08-28 18:09:14 +0900

    delete .eslintignore

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,17 +1,19 @@
-export default [
-  {
-    'env': {
-      'jest': true,
-      'node': true,
-      'browser': true
-    },
-
-    files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
-    rules: {
-        "prefer-const": "warn",
-        "no-constant-binary-expression": "error"
-    }
+export default {
+  env: {
+    jest: true,
+    node: true,
+    browser: true
   },
-  
-];
+  files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+  ignores: [
+    "node_modules",
+    "coverage",
+    "src/__test__"
+  ],
+  rules: {
+    "prefer-const": "warn",
+    "no-constant-binary-expression": "error"
+  }
+};
+
    
