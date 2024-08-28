1bd09baa676b551749fa69c4773d43afea2b3a57
anassstya <408183@edu.itmo.ru>
2024-08-28 18:47:05 +0900

    delete .eslintignore

---
+++ .jest.config.js
@@ -0,0 +1,5 @@
+export const testEnvironment = "node";
+export const moduleFileExtensions = ["js", "json", "jsx", "ts", "tsx"];
+export const transform = {
+  "^.+\\.(js|jsx)$": "babel-jest"
+};
--- src/__test__/test.js
+++ src/__test__/test.js
@@ -4,7 +4,7 @@ test.each([
   [{name: 'Маг1', health: 90}, 'healthy'],
   [{name: 'Маг2', health: 45}, 'wounded'],
   [{name: 'Маг3', health: 10}, 'critical']]
-)('check health', (object, expected) => {
+  )('check health', (object, expected) => {
     const result = check(object);
     expect(result).toBe(expected);
 });
